62e4749fe6f90855529afe9de873de3c4d275cc9
Yichen Jiang <tomathmichelle@gmail.comm>
2017-08-22 00:10:19 +1000

    complete models
    complete models

---
+++ src/java/com/uts/sep/model/BaseModel.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.uts.sep.model;
+
+import java.lang.Object;
+/**
+ *
+ * @author tomat
+ */
+public abstract class BaseModel {
+    public void saveModel(Object object){
+        //
+    }
+    
+    public Object readMedol(){
+        Object object = new Object();
+        //implementation for reading model information.
+        //functions can be edit later for all sub classes.
+        return object;
+    }
+}
---
+++ src/java/com/uts/sep/model/CartItem.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.uts.sep.model;
+
+/**
+ *
+ * @author tomat
+ */
+public class CartItem {
+    int quantity;
+    String size;
+    int itemID;
+    int shoppingCartID;
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public int getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(int itemID) {
+        this.itemID = itemID;
+    }
+
+    public int getShoppingCartID() {
+        return shoppingCartID;
+    }
+
+    public void setShoppingCartID(int shoppingCartID) {
+        this.shoppingCartID = shoppingCartID;
+    }
+
+    public CartItem(int quantity, String size, int itemID, int shoppingCartID) {
+        this.quantity = quantity;
+        this.size = size;
+        this.itemID = itemID;
+        this.shoppingCartID = shoppingCartID;
+    }
+
+    public CartItem() {
+    }
+}
