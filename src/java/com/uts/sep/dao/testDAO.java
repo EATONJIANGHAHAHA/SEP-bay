a7114a3be95d3c9a1f91bd1c36153ff132c41e40
EATONJIANGHAHAHA <eatonjiangtonlei@gmail.com>
2017-09-18 14:17:41 +1000

    BaseDAO working,

--- src/java/com/uts/sep/dao/testDAO.java
+++ src/java/com/uts/sep/dao/testDAO.java
@@ -15,18 +15,18 @@ import java.util.*;
 public class testDAO {
 
     public static void main(String[] args) {
-        ItemDAO dao = new ItemDAO();
-        List<ItemTbl> list = dao.getAll(BaseDAO.ITEM_TBL);
-
-        //dao.insertUser("ZheyiLu", "123456");
-        for (int i = 0; i < list.size(); i++) {
-            ItemTbl item = new ItemTbl();
-            item = list.get(i);
-            System.out.print(item.getItemName());
-            System.out.print(item.getStock());
-            System.out.print(item.getPrice());
-
-        }
+//        ItemDAO dao = new ItemDAO();
+//        List<ItemTbl> list = dao.getAll(BaseDAO.ITEM_TBL);
+//
+//        //dao.insertUser("ZheyiLu", "123456");
+//        for (int i = 0; i < list.size(); i++) {
+//            ItemTbl item = new ItemTbl();
+//            item = list.get(i);
+//            System.out.print(item.getItemName());
+//            System.out.print(item.getStock());
+//            System.out.print(item.getPrice());
+//
+//        }
         
 //        AdminDAO dao = new AdminDAO();
 //        dao.getAll("from AdminTbl");
